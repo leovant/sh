36320d2a402158650a49687603276a50bc28b38d
Leo Tavares <leovant@gmail.com>
2018-04-19 18:57:23 +0100

    Include docker images cleaning.

--- docker/cleanup.sh
+++ docker/cleanup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 docker rm -f $(docker ps -qa)
 docker volume rm $(docker volume ls -q)
+docker rmi -f $(docker images -qa)
 cleanupdirs="/var/lib/etcd /etc/kubernetes /etc/cni /opt/cni /var/lib/cni /var/run/calico"
 for dir in $cleanupdirs; do
   echo "Removing $dir"
